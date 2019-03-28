d28f6748268ffd7669669bc49a84ef5aea5ec810
Croxx <croxxpwn@gmail.com>
2019-03-28 22:59:38 +0800

    update

--- calSim.py
+++ calSim.py
@@ -5,9 +5,8 @@ import os.path
 import numpy as np
 
 
-def nameText(index):
-    # index means index in numpy martix, not the id of the text
-    return 'text_%08d' % (index-1)
+def nameText(i):
+    return 'text_%08d' % (i,)
 
 
 def couP(G, s, t, path):
@@ -17,12 +16,10 @@ def couP(G, s, t, path):
         return 0
     ans = 0
     aim = path[0]
-    adj = [ node for node in G.adj[s] if node[:2] == aim[:2] ]
+    adj = [node for node in G.adj[s] if node[:2] == aim[:2]]
     for a in adj:
-        ans += couP(G,a,t,path[1:])
+        ans += couP(G, a, t, path[1:])
     return ans
-
-
 
 
 if __name__ == "__main__":
@@ -32,43 +29,32 @@ if __name__ == "__main__":
     G = pickle.load(open(os.path.join(baseDir, 'output', 'G.pkl'), 'rb'))
     paths = [['text', 'entity', 'text'], ['text', 'keyword', 'text'], ['text', 'entity', 'entity', 'text'], ['text', 'entity', 'keyword', 'text'], ['text', 'keyword', 'entity', 'text'], ['text', 'keyword', 'keyword', 'text'], ['text', 'entity', 'entity', 'entity', 'text'], ['text', 'entity', 'entity', 'keyword', 'text'], [
         'text', 'entity', 'keyword', 'entity', 'text'], ['text', 'entity', 'keyword', 'keyword', 'text'], ['text', 'keyword', 'entity', 'entity', 'text'], ['text', 'keyword', 'entity', 'keyword', 'text'], ['text', 'keyword', 'keyword', 'entity', 'text'], ['text', 'keyword', 'keyword', 'keyword', 'text']]
-    
-    ts = []
-    es = []
-    ks = []
 
-    for node in G.nodes:
-        if node[:2] == 'te':
-            ts.append(node)
-        elif node[:2] == 'en':
-            es.append(node)
-        elif node[:2] == 'ke':
-            ks.append(node)
-    '''
+    N = 23194
+    train_ids = []
 
-    ts = set()
-    es = set()
-    ks = set()
+    _cs = pickle.load(
+        open(os.path.join(baseDir, 'output', '_codes.pkl'), 'rb'))
 
-    for node in G.nodes:
-        if node[:2] == 'te':
-            ts.add(node)
-        elif node[:2] == 'en':
-            es.add(node)
-        elif node[:2] == 'ke':
-            ks.add(node)
-    '''
+    for c, ts in _cs:
+        if len(train_ids) < N:
+            train_ids.append(ts[0])
+            del ts[0]
 
-    N = len(ts)
-    print(N)
+    train_ids = sorted(train_ids)
+    print(train_ids)
+    pickle.dump(train_ids, os.path.join(baseDir, 'output', 'train_ids.pkl'))
+
     A = np.zeros((14, N, N))
     for index, path in enumerate(paths):
         i = index+1
-        for x in range(1, N+1):
-            for y in range(1, x+1):
+        for x in range(N):
+            for y in range(0, x+1):
+                tx, ty = nameText(train_ids(x)), nameText(train_ids(y))
                 p = path[1:-1]
-                print('Calculating CouP < path=%s , x=%s ,y=%s | total=%s >...' % (i,x,y,N))
-                c = couP(G, nameText(x), nameText(y), p)
-                print('Get Coup(%s,%s)=%s.' % (x,y,c))
-                A[i,x,y] = c
-    pickle.dump(A,open(os.path.join(baseDir,'output','A.pkl'),'wb'))
+                print('Calculating CouP < path=%s , x=%s ,y=%s | total=%s >...' % (
+                    i, tx, ty, N))
+                c = couP(G, tx, ty, p)
+                print('Get Coup(%s,%s)=%s.' % (x, y, c))
+                A[i, x, y] = c
+    pickle.dump(A, open(os.path.join(baseDir, 'output', 'A.pkl'), 'wb'))
